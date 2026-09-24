c6271b7e1ae957330c9a261b00c2aefcf336f765
NinoskaPalmares <palmaresninoska2@gmail.com>
2026-09-23 23:04:20 -0300

    feat: criar classe Aluno e function alunos.js

---
+++ js/Aluno.js
@@ -0,0 +1,22 @@
+
+
+class Aluno {
+    constructor(
+        nome,
+        genero,
+        dataNascimento,
+        cpf,
+        telefone,
+        email,
+        endereco
+    ) {
+        this.nome = nome;
+        this.genero = genero;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+}
+export default Aluno;
---
+++ js/alunos.js
@@ -0,0 +1,24 @@
+import alunos from "../dados/listagem-alunos";
+
+function cadastrarAluno(aluno) {
+    try {
+        let novoId;
+
+        if (alunos.length > 0) {
+            novoId = alunos[alunos.length - 1].id + 1;
+        } else {
+            novoId = 1;
+        }
+
+        aluno.id = novoId;
+
+        alunos.push(aluno);
+
+        return Promise.resolve("Aluno cadastrado com sucesso!");
+    } catch (error) {
+        return Promise.reject("Erro ao cadastrar o aluno");
+    }
+
+}
+
+export default cadastrarAluno;
